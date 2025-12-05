e0e0e0f6e5ca8a5cf9a1c58ee165dca5409d0b66
athacoding-creator <athacoding@gmail.com>
2025-12-05 17:12:44 +0700

    deleted logo home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,6 @@ import hero2 from "@/assets/hero-2.jpg";
 import hero3 from "@/assets/hero-3.jpg";
 import hero4 from "@/assets/hero-4.jpg";
 import hero5 from "@/assets/hero-5.jpg";
-import logoKwp from "@/assets/logo-kwp.png";
 
 interface Kegiatan {
   id: string;
